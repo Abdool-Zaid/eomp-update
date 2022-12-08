05063df57eb55d1c469be48206fe13e0c885611c
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-12-08 14:48:12 +0200

    first commit

---
+++ students.js
@@ -0,0 +1,27 @@
+
+  let temp =[
+{name: 'Tyvon', message: 'currently on admin page'}
+,{name: 'john', message: 'busy with checkout page'}
+,{name: 'Siphamandla', message: 'working on delete btn'}
+,{name: 'Asandile', message: 'on the crud system'}
+,{name: 'mu-een', message: 'crud system'}
+,{name: 'meezaan', message: 'busy with checkout'}
+,{name: 'cassidy', message: 'working on editing'}
+,{name: 'Mkhuseli', message: 'busy with adding '}
+,{name: 'sakhe', message: 'finishing deleting'}
+,{name: 'camryn', message: 'busy with updating and deleting ,needs to do filtering'}
+,{name: 'faeez', message: 'stuck with crud and filtering'}
+,{name: 'brogan', message: 'busy with crud system'}
+,{name: 'javier', message: 'busy with deleting '}
+,{name: 'umayr', message: 'admin page'}
+,{name: 'keenan', message: 'working on admin page next'}
+,{name: 'timothy', message: 'busy with delete '}
+,{name: 'brandon', message: 'busy with checkout'}
+,{name: 'reece', message: 'busy with deleting'}
+,{name: 'maxwell', message: 'busy with filtering'}
+,{name: 'deno', message: 'busy with checkout  functionality'}
+,{name: 'Zimasile', message: 'busy with checkout '}
+,{name: 'luntu', message: 'busy with chekout'}
+,{name: 'gift', message: 'busy with deleting'}
+  ]
+localStorage.setItem('arr',JSON.stringify(temp))
